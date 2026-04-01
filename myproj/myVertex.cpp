ad390b92d394a2815435d89eaa4380aafea5a814
m0ii0m <ludovic.regnier.2004@gmail.com>
2026-04-01 14:10:17 +0200

    TP1 2.

--- myproj/myVertex.cpp
+++ myproj/myVertex.cpp
@@ -17,5 +17,16 @@ myVertex::~myVertex(void)
 
 void myVertex::computeNormal()
 {
-	/**** TODO ****/
+	myHalfedge* step = originof;
+	normal->dX = normal->dY = normal->dZ = 0;
+	int counter = 0;
+
+	do {
+		myVector3D* fn = step->adjacent_face->normal;
+		*normal += *fn;
+		counter++;
+		step = step->twin->next;
+	} while (step != originof);
+
+	*normal = *normal / counter;
 }
